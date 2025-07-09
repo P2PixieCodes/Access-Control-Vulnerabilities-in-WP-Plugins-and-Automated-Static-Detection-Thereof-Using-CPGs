52ab166ce6822b8aee7937f3933d73854877faad
P2PixieCodes <meer.schneid@gmail.com>
2025-07-09 14:01:10 +0200

    [mod] remove comment (worksheet dependencies)

--- queries/util_queries.scala
+++ queries/util_queries.scala
@@ -1,14 +1,13 @@
 import scala.collection.mutable.ListBuffer
-//import $dep.`io.shiftleft::codepropertygraph:1.7.38`
+
 import io.shiftleft.codepropertygraph.generated.*
 import io.shiftleft.codepropertygraph.generated.nodes.*
 import io.shiftleft.codepropertygraph.generated.help.*
 import io.shiftleft.codepropertygraph.generated.language.*
 import io.shiftleft.codepropertygraph.generated.accessors.*
-//import $dep.`io.shiftleft::semanticcpg:1.3.522` //https://mvnrepository.com/artifact/io.shiftleft/semanticcpg_3/1.3.522
 import io.shiftleft.semanticcpg.*
 import io.shiftleft.semanticcpg.language.*
-//import flatgraph.traversal.*
+import flatgraph.traversal.*
 
 /**
   * Given a file <global> method, get all includes/requires of the corresponding file.
